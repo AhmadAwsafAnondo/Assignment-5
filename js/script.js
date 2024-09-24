ffb0cf64b0de074b9ebefd96d2aeea2ecabad6f3
Ahmad Awsaf Anondo <ahmadawsafanondo@gmail.com>
2024-09-24 21:09:05 +0600

    second commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//donate button
+const donateBtn = document.getElementById("donate-btn2");
+donateBtn.addEventListener("click", function () {
+  donateBtn.style.backgroundColor = "rgba(180, 244, 97, 1)";
+  donateBtn.style.border = "none";
+});
+//history button
+const historyBtn = document.getElementById("history-btn");
+historyBtn.addEventListener("click", function () {
+  historyBtn.style.backgroundColor = "rgba(180, 244, 97, 1)";
+  historyBtn.style.border = "none";
+});
